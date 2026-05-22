95bc23fe792ac38e84d485ef32cd10c37e58caa4
Masudur Rahman <45783647+chintitomasud3@users.noreply.github.com>
2026-05-22 17:00:10 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,43 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-07a00cf47dbbc844c"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "masud_server_02"
+  }
+}
+
+
+
 resource "aws_security_group" "example" {
+ 
   name        = "chintito_masud"
   description = "Example security group"
- #SSh Access
+  #SSh Access
   ingress {
     from_port   = 22
     to_port     = 22
@@ -24,18 +54,18 @@ resource "aws_security_group" "example" {
   }
 
   # Http access
- ingress {
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
- ingress {
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
 }
 
